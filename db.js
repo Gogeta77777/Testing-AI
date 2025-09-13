bac858c972ed9937561bc5724db121c046931feb
Gogeta77777 <gogetaishereforever77@gmail.com>
2025-09-13 23:17:25 +1200

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,33 @@
-// This is an in-memory database. It will reset on every serverless function cold start.
-let users = [];
-let messages = [];
+const mongoose = require('mongoose');
 
-module.exports = {
-  users,
-  messages
-};
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error('Please define the MONGODB_URI environment variable');
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+module.exports = dbConnect;
