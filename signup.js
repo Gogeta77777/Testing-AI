8e21d5b7c0b2d10e71fb9fef1d44a94c7b5c0713
Gogeta77777 <gogetaishereforever77@gmail.com>
2025-09-13 23:18:41 +1200

    Create signup.js

---
+++ signup.js
@@ -0,0 +1,37 @@
+const bcrypt = require('bcryptjs');
+const dbConnect = require('../lib/dbConnect');
+const User = require('../models/User');
+
+module.exports = async (req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+
+  try {
+    await dbConnect();
+
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      return res.status(400).json({ message: 'Username and password are required' });
+    }
+
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(409).json({ message: 'User already exists' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = new User({
+      username,
+      password: hashedPassword,
+    });
+
+    await user.save();
+
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
